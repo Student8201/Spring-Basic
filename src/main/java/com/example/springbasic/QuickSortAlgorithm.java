3d55f99604bcd54330e21770ad720564413c2dfa
student8201 <student8201@gmail.com>
2019-06-07 00:17:21 +0700

    many component beans @Primary

--- src/main/java/com/example/springbasic/QuickSortAlgorithm.java
+++ src/main/java/com/example/springbasic/QuickSortAlgorithm.java
@@ -1,10 +1,12 @@
 package com.example.springbasic;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import com.example.springbasic.service.SortAlgorithm;
 
-
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
   
   public int[] sort(int[] numbers) {
